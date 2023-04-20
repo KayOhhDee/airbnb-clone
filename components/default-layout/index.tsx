653e88387680931481e00dd8ddd1f7e5842682da
Kwasi Owusu-Donkor <kod3180@gmail.com>
2023-04-20 01:20:44 +0000

    add toaster provider and register modal component to layout

--- components/default-layout/index.tsx
+++ components/default-layout/index.tsx
@@ -1,12 +1,16 @@
 'use client';
 import { Nunito } from 'next/font/google'
 import Navbar from './navbar'
+import RegisterModal from '../modals/RegisterModal';
+import ToasterProvider from '@/providers/ToasterProvider';
 
 const nunito = Nunito({ subsets: ['latin'] })
 
 export default function DefaultLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <ToasterProvider />
+      <RegisterModal />
       <Navbar />
       <main className={nunito.className}>
         {children}
