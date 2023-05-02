330dfee2d20ab322d28201560a8d143d2b156269
Kwasi Owusu-Donkor <kod3180@gmail.com>
2023-05-02 23:25:38 +0000

    provide session to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import DefaultLayout from '@/components/default-layout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from "next-auth/react"
 
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps }}: AppProps) {
   return (
+    <SessionProvider session={session}>
       <DefaultLayout>
         <Component {...pageProps} />
       </DefaultLayout>
+    </SessionProvider>
   )
 }
