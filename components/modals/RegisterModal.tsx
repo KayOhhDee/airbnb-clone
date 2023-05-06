159761f303c370a53119c9dcae20973a140a9146
Kwasi Owusu-Donkor <kod3180@gmail.com>
2023-05-06 00:34:47 +0000

    implement signin and sign up with github

--- components/modals/RegisterModal.tsx
+++ components/modals/RegisterModal.tsx
@@ -13,6 +13,7 @@ import Heading from '../Heading';
 import Input from '../inputs/input';
 import { toast } from 'react-hot-toast';
 import Button from '../Button';
+import { signIn } from 'next-auth/react';
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal()
@@ -83,7 +84,7 @@ const RegisterModal = () => {
     <div className='flex flex-col gap-4 mt-3'>
       <hr />
       <Button outline label='Continue with Google' icon={FcGoogle} onClick={() => {}} />
-      <Button outline label='Continue with Github' icon={AiFillGithub} onClick={() => {}} />
+      <Button outline label='Continue with Github' icon={AiFillGithub} onClick={() => signIn('github')} />
       <div className="text-neutral-500 text-center mt-4 font-light">
         <div className='flex flex-row item-center justify-center gap-2'>
           <div>
