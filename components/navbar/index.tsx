9a6a6964b885a38b42357729cf1db97eb1edbb50
Kwasi Owusu-Donkor <kod3180@gmail.com>
2023-05-11 23:43:09 +0000

    setup categories and category box components

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Container from "../Container";
+import Categories from "./Categories";
 import Logo from "./Logo";
 import SearchFilter from "./SearchFilter";
 import UserMenu from "./UserMenu";
@@ -22,6 +23,7 @@ const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
           </div>
         </Container>
       </div>
+      <Categories />
     </div>
   );
 }
